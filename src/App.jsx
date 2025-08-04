264477c2364fbf68938403f777f581350c39bdd3
kozyyy <samkankam296@gmail.com>
2025-08-04 15:25:24 +0000

    face verification repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, {useEffect, useState, useRef} from 'react';
+
+function FaceVerification(){
+  return (
+  <div className='face-verification-container'>
+    <div className='verification-card'>
+      <h1>Take a Selfie for</h1>
+      <h2>Selfie Verification</h2>
+      <div className="camera-container">
+        navigator.
+      </div>
+    </div>
+
+  </div>
+
+  );
+}
